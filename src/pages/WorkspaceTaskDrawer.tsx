979ce8ff8d5089f57308450b3cb884b507cb27fd
DEV-trongphuc <DEV-trongphuc@users.noreply.github.com>
2026-08-08 23:37:44 +0700

    feat: move Pomodoro Focus Tracker to system Navbar Header with task selection popover

--- src/pages/WorkspaceTaskDrawer.tsx
+++ src/pages/WorkspaceTaskDrawer.tsx
@@ -23,7 +23,6 @@ import { useUIStore } from '../store/uiStore';
 import { useUploadProgress } from '../contexts/UploadProgressContext';
 import { PasteDropzoneArea } from '../components/ui/PasteDropzoneArea';
 import { ConfirmModal } from '../components/ui/ConfirmModal';
-import { PomodoroTracker } from '../components/PomodoroTracker';
 
 interface WorkspaceTaskDrawerProps {
   isOpen: boolean;
@@ -4994,11 +4993,6 @@ export const WorkspaceTaskDrawer: React.FC<WorkspaceTaskDrawerProps> = ({
                 }}
               />
             </div>
-
-            {/* Pomodoro Focus Tracker */}
-            {task && task.id && (
-              <PomodoroTracker taskId={task.id} />
-            )}
 
             {/* Lặp lại định kỳ */}
             <div className="card" style={cardStyle}>
